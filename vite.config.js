c5d592005ade87d1979d79157268c42bba29f2b1
Elvis Chen <elvischen291@gmail.com>
2024-05-01 22:32:22 -0500

    reformat base

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ command, mode }) => {
     server: {
       port: 5174,
     },
-    base: "./",
+    base: "/project-3-full-stack-agile-web-902-team-3/",
     resolve: {
       alias: [
         {
